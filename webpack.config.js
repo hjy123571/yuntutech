8e3c7a081926001475e3a7808d3003daaed0bebe
codeknight123 <codeknight123@gmail.com>
2024-03-16 23:48:36 +0800

    add image optimizer in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -19,6 +21,12 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+        },
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
@@ -33,12 +41,36 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(jpg|jpeg|png)$/,
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
         use: {
           loader: "file-loader",
           options: {
             name: "[name].[ext]",
             outputPath: "images/", // 设置输出的图片文件夹
+          },
+        },
+      },
+      {
+        loader: "image-webpack-loader",
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 65, // Set the quality for JPEG images
+          },
+          // Optipng.enabled: false will disable optipng
+          optipng: {
+            enabled: false,
+          },
+          pngquant: {
+            quality: [0.65, 0.9],
+            speed: 4,
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+          // the webp option will enable WEBP
+          webp: {
+            quality: 75,
           },
         },
       },
@@ -50,6 +82,12 @@ module.exports = {
         test: /\.svg$/,
         type: "asset/resource",
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(), //  CSS 压缩插件
+      new TerserPlugin(), // JS 压缩插件
     ],
   },
 };
